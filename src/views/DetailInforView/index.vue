ed0b29db005b1b6d82ae8585032fb9d5d4ceeb53
ngxuantai <xuantai1902@gmail.com>
2024-04-15 15:43:45 +0700

    Detail wine view

--- src/views/DetailInforView/index.vue
+++ src/views/DetailInforView/index.vue
@@ -11,6 +11,92 @@
             Back
           </a-button>
         </div>
+        <div class="wine-infor">
+          <div class="img-container">
+            <img src="../../assets/image/img.png" alt="item-image" />
+          </div>
+          <div class="information">
+            <div class="infor">
+              <h3>60 Sessantanni Limited Edition Italia champagne</h3>
+            </div>
+            <div class="detail">
+              <a-row class="detail-row-first">
+                <a-col :span="8">
+                  <div class="detail-content">
+                    <img src="../../assets/icon/Grapes.png" alt="grapes" />
+                    <span>Primitivo</span>
+                  </div>
+                </a-col>
+                <a-col :span="8">
+                  <div class="detail-content">
+                    <img src="../../assets/icon/Grapes.png" alt="grapes" />
+                    <span>Primitivo</span>
+                  </div>
+                </a-col>
+                <a-col :span="8">
+                  <div class="detail-content">
+                    <img src="../../assets/icon/Grapes.png" alt="grapes" />
+                    <span>Primitivo</span>
+                  </div>
+                </a-col>
+              </a-row>
+              <a-row class="detail-row-second">
+                <a-col :span="12">
+                  <div class="detail-content">
+                    <img src="../../assets/icon/Grapes.png" alt="grapes" />
+                    <span>Primitivo</span>
+                  </div>
+                </a-col>
+                <a-col :span="12">
+                  <div class="detail-content">
+                    <img src="../../assets/icon/Grapes.png" alt="grapes" />
+                    <span>Primitivo</span>
+                  </div>
+                </a-col>
+              </a-row>
+            </div>
+            <div class="description">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit
+              </p>
+            </div>
+            <div class="add-container">
+              <div class="wine-capacity">
+                <div>
+                  <img src="../../assets/icon/Champagne.png" alt="champagne" />
+                  150ml
+                </div>
+                <div>
+                  <img
+                    src="../../assets/icon/BeerBottle.png"
+                    alt="beerbottle"
+                  />
+                  750ml
+                </div>
+              </div>
+              <div class="add">
+                <h4>From <span>$300</span></h4>
+                <a-button>+</a-button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="story">
+          <h3>The story of origin</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore
+          </p>
+        </div>
       </div>
     </a-layout>
   </a-layout>
